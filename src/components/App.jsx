5b7bd910c99333c1ef2bf5d69b3c83bc224cceea
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-12-12 16:14:21 +0200

    summ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      query: '',
-      images: [],
-      currentPage: 1,
-      largeImageURL: '',
-      showModal: false,
-      hasMoreImages: true,
-      loading: false,
-    };
-  }
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [hasMoreImages, setHasMoreImages] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  handleSearchSubmit = (searchQuery) => {
-    this.setState({
-      query: searchQuery,
-      images: [],
-      currentPage: 1,
-      hasMoreImages: true,
-    }, () => this.fetchImages(searchQuery, 1));
-  };
+  const handleSearchSubmit = useCallback((searchQuery) => {
+    setQuery(searchQuery);
+    setImages([]);
+    setCurrentPage(1);
+    setHasMoreImages(true);
+    fetchImages(searchQuery, 1);
+  }, []);
 
-fetchImages = (searchQuery, page) => {
-  this.setState({ loading: true });
+  const fetchImages = useCallback((searchQuery, page) => {
+    setLoading(true);
 
-  const apiKey = '40243094-9cac1343afd7c4b92bc3dbcfd';
-  const perPage = 12;
-  const apiUrl = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
+    const apiKey = '40243094-9cac1343afd7c4b92bc3dbcfd';
+    const perPage = 12;
+    const apiUrl = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const { hits, totalHits } = data;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const { hits, totalHits } = data;
 
-      this.setState((prevState) => ({
-        images: [...prevState.images, ...hits],
-        currentPage: prevState.currentPage + 1,
-        hasMoreImages: prevState.currentPage < Math.ceil(totalHits / perPage),
-      }));
-    })
-    .catch((error) => console.error('Error fetching images:', error))
-    .finally(() => {
-      this.setState({ loading: false });
-    });
-};
+        setImages((prevImages) => [...prevImages, ...hits]);
+        setCurrentPage((prevPage) => prevPage + 1);
+        setHasMoreImages(currentPage < Math.ceil(totalHits / perPage));
+      })
+      .catch((error) => console.error('Error fetching images:', error))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [currentPage]);
 
-handleLoadMore = () => {
-  const { loading, hasMoreImages, query, currentPage } = this.state;
+  const handleLoadMore = useCallback(() => {
+    if (!loading && hasMoreImages) {
+      fetchImages(query, currentPage);
+    }
+  }, [loading, hasMoreImages, query, currentPage, fetchImages]);
 
-  if (!loading && hasMoreImages) {
-    this.fetchImages(query, currentPage);
-  }
-};
+  const openModal = useCallback((largeURL) => {
+    setLargeImageURL(largeURL);
+    setShowModal(true);
+  }, []);
 
-
-  openModal = (largeURL) => {
-    this.setState({
-      largeImageURL: largeURL,
-      showModal: true,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      largeImageURL: '',
-      showModal: false,
-    });
-  };
-
-render() {
-  const { images, largeImageURL, showModal, hasMoreImages, loading } = this.state;
-  const shouldRenderLoadMore = images.length > 0 && hasMoreImages;
+  const closeModal = useCallback(() => {
+    setLargeImageURL('');
+    setShowModal(false);
+  }, []);
 
   return (
     <div className="App">
-      <Searchbar onSubmit={this.handleSearchSubmit} />
-      <ImageGallery images={images} openModal={this.openModal} />
+      <Searchbar onSubmit={handleSearchSubmit} />
+      <ImageGallery images={images} openModal={openModal} />
       {loading && <p>Loading...</p>}
-       {shouldRenderLoadMore && !loading && <Button onLoadMore={this.handleLoadMore} hasMoreImages={hasMoreImages} />}
-      {showModal && <Modal largeImageURL={largeImageURL} onClose={this.closeModal} />}
+      {images.length > 0 && hasMoreImages && !loading && (
+        <Button onLoadMore={handleLoadMore} hasMoreImages={hasMoreImages} />
+      )}
+      {showModal && <Modal largeImageURL={largeImageURL} onClose={closeModal} />}
     </div>
   );
-}
-}
+};
 
 export default App;
