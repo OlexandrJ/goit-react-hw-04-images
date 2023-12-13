e8ac6d20d9b19c51ae49d314831c0557c1669f90
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-12-13 11:21:59 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
@@ -13,15 +13,15 @@ const App = () => {
   const [hasMoreImages, setHasMoreImages] = useState(true);
   const [loading, setLoading] = useState(false);
 
-const handleSearchSubmit = useCallback((searchQuery) => {
-  setQuery(searchQuery);
-  setImages([]);
-  setCurrentPage(1);
-  setHasMoreImages(true);
-  // fetchImages(searchQuery, 1);
-}, []);
+  const handleSearchSubmit = (searchQuery) => {
+    setQuery(searchQuery);
+    setImages([]);
+    setCurrentPage(1);
+    setHasMoreImages(true);
+    fetchImages(searchQuery, 1);
+  };
 
-  const fetchImages = useCallback((searchQuery, page) => {
+  const fetchImages = (searchQuery, page) => {
     setLoading(true);
 
     const apiKey = '40243094-9cac1343afd7c4b92bc3dbcfd';
@@ -41,32 +41,32 @@ const handleSearchSubmit = useCallback((searchQuery) => {
       .finally(() => {
         setLoading(false);
       });
-  }, [currentPage]);
+  };
 
-  const handleLoadMore = useCallback(() => {
+  const handleLoadMore = () => {
     if (!loading && hasMoreImages) {
       fetchImages(query, currentPage);
     }
-  }, [loading, hasMoreImages, query, currentPage, fetchImages]);
+  };
 
-  const openModal = useCallback((largeURL) => {
+  const openModal = (largeURL) => {
     setLargeImageURL(largeURL);
     setShowModal(true);
-  }, []);
+  };
 
-  const closeModal = useCallback(() => {
+  const closeModal = () => {
     setLargeImageURL('');
     setShowModal(false);
-  }, []);
+  };
+
+  const shouldRenderLoadMore = images.length > 0 && hasMoreImages;
 
   return (
     <div className="App">
       <Searchbar onSubmit={handleSearchSubmit} />
       <ImageGallery images={images} openModal={openModal} />
       {loading && <p>Loading...</p>}
-      {images.length > 0 && hasMoreImages && !loading && (
-        <Button onLoadMore={handleLoadMore} hasMoreImages={hasMoreImages} />
-      )}
+      {shouldRenderLoadMore && !loading && <Button onLoadMore={handleLoadMore} hasMoreImages={hasMoreImages} />}
       {showModal && <Modal largeImageURL={largeImageURL} onClose={closeModal} />}
     </div>
   );
